40754986ba0f71687624cffd5565ad35276eb3b1
Abderrazak <aamaichabderrazak2004@gmail.com>
2025-11-20 21:55:44 +0100

    J'ai oublier de travallier sur mon branch Abderrazak et j'ai ajouter la fonction daffichage un employee dans sideBar

--- script.js
+++ script.js
@@ -53,12 +53,10 @@ let idcount = 1;
 let countinerExperience = document.getElementById("countinerExperience");
 formAjouterStaff = document.forms["formAjouterStaff"];
 
-// console.log(formAjouterStaff);
-
 formAjouterStaff.addEventListener("submit", (e) => {
   e.preventDefault();
 
-   let nomExp = document.querySelectorAll(".nomExper");
+  let nomExp = document.querySelectorAll(".nomExper");
   let roleExp = document.querySelectorAll(".roleExper");
   let arrayExper = [];
 
@@ -74,7 +72,6 @@ formAjouterStaff.addEventListener("submit", (e) => {
     exper: arrayExper,
   };
   // Array experience
- 
 
   for (let i = 0; i < nomExp.length; i++) {
     arrayExper.push({
@@ -93,6 +90,7 @@ formAjouterStaff.addEventListener("submit", (e) => {
   icon_cover.style.display = "block";
 
   alert("Employé ajouté avec succès!");
+  location.reload();
 });
 // load data from localStorage
 let data = localStorage.getItem("staffData");
@@ -112,3 +110,51 @@ input_photo.addEventListener("input", function () {
   cover_photo.style.backgroundImage = `url(${input_photo.value})`;
   icon_cover.style.display = "none";
 });
+
+// -----------ajouter employee dans aside
+function afficherStaff() {
+  let sidBar = document.getElementById("side-bar");
+  let new_Employee = document.querySelectorAll(".new_Employee");
+  new_Employee = document.createElement("div");
+  sidBar.appendChild(new_Employee);
+  new_Employee.innerHTML= "";
+
+  ArrayStaff.forEach((employe) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+       <div class=" flex justify-between bg-slate-200 rounded-md py-2 my-2 px-1">
+          <div class="flex justify-evenly gap-1">
+            <div>
+              <img class="w-12 rounded-full" src="${employe.photo}" alt="" />
+            </div>
+            <div class="flex flex-col px-1">
+              <label for="nom">${employe.nom}</label>
+              <label for="poste" class="text-red-600">${employe.prenom}</label>
+            </div>
+          </div>
+          <div class="flex items-center">
+            <svg
+              class="w-9 text-gray-800 dark:text-black"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="square"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 19H5a1 1 0 0 1-1-1v-1a3 3 0 0 1 3-3h1m4-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm7.441 1.559a1.907 1.907 0 0 1 0 2.698l-6.069 6.069L10 19l.674-3.372 6.07-6.07a1.907 1.907 0 0 1 2.697 0Z"
+              />
+            </svg>
+          </div>
+        </div>`;
+    new_Employee.appendChild(div);
+  });
+  
+}
+  
+afficherStaff();
