3a1ae37b3901a4ed77bcf2c1b410fb5a70610063
Abderrazak <aamaichabderrazak2004@gmail.com>
2025-11-20 10:47:44 +0100

    jai fait lajout des employee sur tableau

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ document.getElementById('btn_Ajouter_Exper').addEventListener("click", ()=>{
                       <div class="flex flex-col">
                         <label for="name_Expe">Nom</label>
                         <input
+                          id="nomExper"
                           type="text"
                           class="rounded-lg py-2 px-1 bg-gray-700/25"
                           placeholder="Nom"
@@ -19,6 +20,7 @@ document.getElementById('btn_Ajouter_Exper').addEventListener("click", ()=>{
                       <div class="flex flex-col">
                         <label for="name_Expe">Role</label>
                         <input
+                          id="roleExper"
                           type="text"
                           class="rounded-lg py-2 px-1 bg-gray-700/25"
                           placeholder="role"
@@ -36,3 +38,39 @@ function formValidation(){
 }
 
 // Ajouter staff
+let ArrayStaff = [];
+formAjouterStaff = document.forms["formAjouterStaff"];
+formAjouterStaff.addEventListener("submit", function (e) {
+   
+    e.preventDefault()
+    console.log(11);
+    let employee = {
+        photo : formAjouterStaff.input_photo.value,
+        nom : formAjouterStaff.lastname.value,
+        prenom : formAjouterStaff.firstname.value,
+        email : formAjouterStaff.email.value,
+        telephone : formAjouterStaff.telephone.value,
+        exper : [
+            {
+                nom : formAjouterStaff.nomExper.value,
+                role : formAjouterStaff.roleExper.value,
+            }
+        ]
+    }
+      console.log(employee);
+    ArrayStaff.push(employee);
+    console.log(ArrayStaff);
+    formAjouterStaff.reset();
+    
+});
+
+
+// photo modal
+let input_photo = document.getElementById('input_photo');
+let cover_photo = document.getElementById('cover_photo');
+let icon_cover = document.getElementById('icon_cover');
+input_photo.addEventListener("input",function(){
+   cover_photo.style.backgroundImage = `url(${input_photo.value})`;
+   icon_cover.style.display = 'none';
+})
+
